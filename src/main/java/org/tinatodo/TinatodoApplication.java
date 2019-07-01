882bea75232ec87953c5184b0bc2cc8cc22f1e04
Thiago Oliveira <thiago.tgo90@gmail.com>
2019-07-01 20:47:36 +0100

    Initial commit

---
+++ src/main/java/org/tinatodo/TinatodoApplication.java
@@ -0,0 +1,13 @@
+package org.tinatodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinatodoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinatodoApplication.class, args);
+	}
+
+}
