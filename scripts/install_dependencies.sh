9caa19563da3a6bc4807d690518359e362462a1c
Thiago Oliveira <thiago.tgo90@gmail.com>
2019-08-22 07:37:29 +0100

    using S3

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo installing maven...
-sudo yum update -y
-sudo yum install -y maven
-sudo yum install -y git-all
+# sudo yum update -y
+# sudo yum install -y maven
+# sudo yum install -y git-all
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# git clone --single-branch --branch backend-endpoints https://github.com/thiagotgo90/TINATODO.git
-# cd TINATODO
-# mvn install
+git clone --single-branch --branch backend-endpoints https://github.com/thiagotgo90/TINATODO.git
+cd TINATODO
+mvn install
 # java -jar target/tinatodo-0.0.1-SNAPSHOT.jar
